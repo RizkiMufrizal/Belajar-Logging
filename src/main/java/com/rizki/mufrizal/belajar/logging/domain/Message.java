b63b36537620408b0f1f6dc4677c3620592b9149
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-11 17:36:36 +0700

    Add Domain Class

---
+++ src/main/java/com/rizki/mufrizal/belajar/logging/domain/Message.java
@@ -0,0 +1,73 @@
+package com.rizki.mufrizal.belajar.logging.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+
+@Entity
+@Table(name = "tb_message")
+public class Message implements Serializable{
+	
+	private static final long serialVersionUID = -6093321383634839287L;
+
+	@Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+	@Column(name = "idMessage")
+	private String idMessage;
+	
+	@Column(name = "message", length = 30)
+	private String message;
+	
+	@Column(name = "tujuan", length = 30)
+	private String tujuan;
+
+	@JsonFormat(shape = Shape.STRING, pattern = "dd/mm/yyyy")
+	@Temporal(TemporalType.DATE)
+	@Column(name = "tanggalMasuk")
+	private Date tanggalMasuk;
+	
+	public String getIdMessage() {
+		return idMessage;
+	}
+	
+	public void setIdMessage(String idMessage) {
+		this.idMessage = idMessage;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getTujuan() {
+		return tujuan;
+	}
+	
+	public void setTujuan(String tujuan) {
+		this.tujuan = tujuan;
+	}
+	
+	public Date getTanggalMasuk() {
+		return tanggalMasuk;
+	}
+
+	public void setTanggalMasuk(Date tanggalMasuk) {
+		this.tanggalMasuk = tanggalMasuk;
+	}
+}
