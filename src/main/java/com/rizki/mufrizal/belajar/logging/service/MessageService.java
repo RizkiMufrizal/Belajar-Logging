69e4a8e4b3902d04fcdae8be577ee7d450dc6d60
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-12 08:34:54 +0700

    Format Code Service

--- src/main/java/com/rizki/mufrizal/belajar/logging/service/MessageService.java
+++ src/main/java/com/rizki/mufrizal/belajar/logging/service/MessageService.java
@@ -1,13 +1,20 @@
 package com.rizki.mufrizal.belajar.logging.service;
 
+import java.util.Date;
 import java.util.List;
 
 import com.rizki.mufrizal.belajar.logging.domain.Message;
 
 public interface MessageService {
-	public void save(Message message);
-	public void update(Message message);
 	public void delete(Message message);
+
+	public Message findByTanggalMasuk(Date date);
+
 	public Message getMessage(String idMessage);
+
 	public List<Message> getMessages();
+
+	public void save(Message message);
+
+	public void update(Message message);
 }
