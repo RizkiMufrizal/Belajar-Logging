a01084a59a3ebd26f848856bb73c3c4d276409b2
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-12 08:33:37 +0700

    Update Domain Class

--- src/main/java/com/rizki/mufrizal/belajar/logging/domain/Message.java
+++ src/main/java/com/rizki/mufrizal/belajar/logging/domain/Message.java
@@ -18,19 +18,19 @@ import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
 @Entity
 @Table(name = "tb_message")
-public class Message implements Serializable{
-	
+public class Message implements Serializable {
+
 	private static final long serialVersionUID = -6093321383634839287L;
 
 	@Id
-    @GeneratedValue(generator = "system-uuid")
-    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+	@GeneratedValue(generator = "system-uuid")
+	@GenericGenerator(name = "system-uuid", strategy = "uuid")
 	@Column(name = "idMessage")
 	private String idMessage;
-	
+
 	@Column(name = "message", length = 30)
 	private String message;
-	
+
 	@Column(name = "tujuan", length = 30)
 	private String tujuan;
 
@@ -38,36 +38,36 @@ public class Message implements Serializable{
 	@Temporal(TemporalType.DATE)
 	@Column(name = "tanggalMasuk")
 	private Date tanggalMasuk;
-	
+
 	public String getIdMessage() {
 		return idMessage;
 	}
-	
-	public void setIdMessage(String idMessage) {
-		this.idMessage = idMessage;
-	}
-	
+
 	public String getMessage() {
 		return message;
 	}
-	
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	
-	public String getTujuan() {
-		return tujuan;
-	}
-	
-	public void setTujuan(String tujuan) {
-		this.tujuan = tujuan;
-	}
-	
+
 	public Date getTanggalMasuk() {
 		return tanggalMasuk;
 	}
 
+	public String getTujuan() {
+		return tujuan;
+	}
+
+	public void setIdMessage(String idMessage) {
+		this.idMessage = idMessage;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
 	public void setTanggalMasuk(Date tanggalMasuk) {
 		this.tanggalMasuk = tanggalMasuk;
+	}
+
+	public void setTujuan(String tujuan) {
+		this.tujuan = tujuan;
 	}
 }
