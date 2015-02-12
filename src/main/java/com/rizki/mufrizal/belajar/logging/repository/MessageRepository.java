e24f6b4a3eaa5ee3b335b0c0a2288702b517d731
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-12 08:34:35 +0700

    Format Code Repository

--- src/main/java/com/rizki/mufrizal/belajar/logging/repository/MessageRepository.java
+++ src/main/java/com/rizki/mufrizal/belajar/logging/repository/MessageRepository.java
@@ -1,11 +1,13 @@
 package com.rizki.mufrizal.belajar.logging.repository;
 
+import java.util.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.rizki.mufrizal.belajar.logging.domain.Message;
 
 @Repository
-public interface MessageRepository extends JpaRepository<Message, String>{
-
+public interface MessageRepository extends JpaRepository<Message, String> {
+	public Message findBytanggalMasuk(Date date);
 }
