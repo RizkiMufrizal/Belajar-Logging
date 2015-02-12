3d4e4784dc987e7c9698353a0e42c4e59fe5951b
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-02-12 08:32:36 +0700

    Update Main Class

--- src/main/java/com/rizki/mufrizal/belajar/logging/WebApplication.java
+++ src/main/java/com/rizki/mufrizal/belajar/logging/WebApplication.java
@@ -8,12 +8,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@EntityScan(basePackages = {"com.rizki.mufrizal.belajar.logging.domain"})
-@EnableJpaRepositories(basePackages = {"com.rizki.mufrizal.belajar.logging.repository"})
+@EntityScan(basePackages = { "com.rizki.mufrizal.belajar.logging.domain" })
+@EnableJpaRepositories(basePackages = { "com.rizki.mufrizal.belajar.logging.repository" })
 public class WebApplication {
 
-	public static void main(String[]args){
+	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
-	
+
 }
